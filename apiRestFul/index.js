a231148b210aaa71dbdb817930edbc7ce5c0b2dd
roberto fabian ponczko <fabianponczko@live.com.ar>
2022-10-09 21:55:36 -0300

    apirestful para trabajo practico productos

--- apiRestFul/index.js
+++ apiRestFul/index.js
@@ -1,15 +1,14 @@
 const express = require (`express`)
 const app = express()
 
-const personas = require('./Routes/personas.js')
-const mascotas = require('./Routes/mascotas.js')
-
+const productos = require('./routes/productos.js')
 
 const server = app.listen(8080,()=>console.log('server iniciado en puerto 8080'))
-server.on('error', err => console.log(`Error on server ${err}`));
+server.on('error', err => console.log(`Error en server: ${err}`));
 
-//para poder leer los body
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.use('/mascotas', mascotas)
-app.use('/personas', personas)
+app.use('/static/',express.static(__dirname+'/public'))
+
+app.use('/',productos)
