4e61aa1dd544a5a0902ae3aecb4dc9fca64fae59
roberto fabian ponczko <fabianponczko@live.com.ar>
2022-12-08 21:15:24 -0300

    trabajo finalizado todo funcional

--- apiRestFul/routes/destroy.js
+++ apiRestFul/routes/destroy.js
@@ -3,15 +3,23 @@ const router = express.Router()
 const session = require('express-session')
 
 
-router.get('/destroy',(req,res)=>{
+router.get('/destroy',async (req,res)=>{
     const userName=req.session.nombre
     console.log("se elimino session ", userName)
     req.session.destroy()    
-    res.render('logout.hbs',{userName})
     
-    setTimeout(()=>{
-        res.redirect("/login")
-    }, 2000);
+    
+        res.render('logout.hbs',{userName})
+        res.setHeader('Content-Type', 'text/html');
+        
+    res.setTimeout(3000,function(){
+        res.render('loginUser.hbs')
+    })
+
+
+
+        // call back function is called when request timed out.
+
 })
 
 module.exports = router
