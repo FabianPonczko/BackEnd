b8bbcf03974d506e2b1dccaa6fb35ae93faa45b8
Fabian <102450602+FabianPonczko@users.noreply.github.com>
2023-01-18 10:44:11 -0300

    Delete entrega1 directory

--- entrega1/src/Dao/index.js
+++
@@ -1,9 +0,0 @@
-import {Container} from '../Containers/index.js'
-
-const PRODUCTS_FILENAME = 'products'
-const CART_FILENAME = 'cart'
-
-const productsDao = new Container(PRODUCTS_FILENAME)
-const cartDao = new Container(CART_FILENAME)
-
-export {productsDao,cartDao}
