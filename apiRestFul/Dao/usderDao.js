8dd02575145611264dd7c782359362a4a01b612f
FabianPonczko <fabianponczko@live.com.ar>
2023-01-20 10:00:52 -0300

    sin userdao

--- apiRestFul/Dao/usderDao.js
+++ apiRestFul/Dao/usderDao.js
@@ -1,12 +1,12 @@
 
-const  MongoDbContainer  =require ("./../Containers/MongoDbContainer.js");
-const { UserModel} =require('./../Models/UserModel')
+// const  MongoDbContainer  =require ("./../Containers/MongoDbContainer.js");
+// const { UserModel} =require('./../Models/UserModel')
 
-module.exports = class UsersMongo extends MongoDbContainer{
-    constructor(){
-        super({
-            name:UserModel.UserCollection,
-            schema:UserModel.UserSchema
-        })
-    }
-}
+// module.exports = class UsersMongo extends MongoDbContainer{
+//     constructor(){
+//         super({
+//             name:UserModel.UserCollection,
+//             schema:UserModel.UserSchema
+//         })
+//     }
+// }
